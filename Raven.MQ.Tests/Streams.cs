77a671d293afa6b4b9853a718cc04d783b6f64c9
Ayende Rahien <Ayende@ayende.com>
2010-11-09 16:02:58 +0200

    Adding tests for streams behavior

---
+++ Raven.MQ.Tests/Streams.cs
@@ -0,0 +1,36 @@
+using System;
+using RavenMQ.Data;
+using Xunit;
+using System.Linq;
+
+namespace Raven.MQ.Tests
+{
+    public class Streams : AbstractQueuesTest
+    {
+        [Fact]
+        public void Can_add_and_get_item_to_stream()
+        {
+            queues.Enqueue(new IncomingMessage
+            {
+                Queue = "/streams/customers/1234",
+                Data = new byte[]{1,2,3},
+            });
+
+            Assert.NotNull(queues.Read("/streams/customers/1234", Guid.Empty).First());
+        }
+
+        [Fact]
+        public void Can_read_item_more_than_once()
+        {
+            queues.Enqueue(new IncomingMessage
+            {
+                Queue = "/streams/customers/1234",
+                Data = new byte[] { 1, 2, 3 },
+            });
+
+            Assert.NotNull(queues.Read("/streams/customers/1234", Guid.Empty).First());
+            Assert.NotNull(queues.Read("/streams/customers/1234", Guid.Empty).First());
+            Assert.NotNull(queues.Read("/streams/customers/1234", Guid.Empty).First());
+        }
+    }
+}
