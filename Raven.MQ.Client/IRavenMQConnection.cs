e33bca55837112be2cf90b7f0c65155bc89ffe94
Ayende Rahien <ayende@ayende.com>
2011-01-14 17:26:46 +0200

    Adding tryout project

---
+++ Raven.MQ.Client/IRavenMQConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Raven.Abstractions.Data;
+
+namespace Raven.MQ.Client
+{
+	public interface IRavenMQConnection : IDisposable
+	{
+		Task ConnectToServerTask { get; }
+		IDisposable Subscribe(string queue, Action<IRavenMQContext, OutgoingMessage> action);
+		Task PublishAsync(IncomingMessage msg);
+		Task PublishMessagesAsync(IEnumerable<IncomingMessage> msgs);
+	}
+}
---
+++ Raven.MQ.Tryouts/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Abstractions.Data;
+using Raven.MQ.Client;
+
+namespace Raven.MQ.Tryouts
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using(var connection = RavenMQConnection.Connect("http://reduction:8181"))
+			{
+				connection.Subscribe("/queues/abc", (context, message) => 
+					Console.WriteLine(Encoding.UTF8.GetString(message.Data)));
+
+				connection.PublishAsync(new IncomingMessage
+				{
+					Queue = "/queues/abc",
+					Data = Encoding.UTF8.GetBytes("Hello Ravens")
+				});
+
+				Console.ReadLine();
+			}
+		}
+	}
+}
